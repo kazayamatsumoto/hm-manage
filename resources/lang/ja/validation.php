f2dd0a9b0b1933757c3a9d77625c5d3c88b13814
kazayamatsumoto <kazayabank@gmail.com>
2022-11-15 21:35:04 +0900

    ユーザー登録機能実装 ユーザー登録画面日本語化

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -118,6 +118,14 @@ return [
     'url' => 'The :attribute format is invalid.',
     'uuid' => 'The :attribute must be a valid UUID.',
 
+
+    'attributes' => [
+        'name'=>'名前', //追記
+        'email'=>'メールアドレス', //追記
+        'password'=>'パスワード', //追記
+        'self_introduction'=>'自己紹介文', //追記
+    ],
+    
     /*
     |--------------------------------------------------------------------------
     | Custom Validation Language Lines
