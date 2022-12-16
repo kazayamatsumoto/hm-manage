b68ee35b52d7b327992109217989c3b5972e5c7a
kazayamatsumoto <kazayabank@gmail.com>
2022-12-16 23:23:42 +0900

    工数管理トップメニュー追加

---
+++ resources/lang/ja/message.php
@@ -0,0 +1,380 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    |日本語コメント対応
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    /*
+    | ボタン、タイトル、ラベル
+    */
+    /* ステータス **/
+    'NEW' => '新規登録', 
+    'EDIT' => '編集中',
+    'REGIST_IN_PROGRESS' => '申請中',
+    'REGIST_IN_PROGRESS_RETRY' => '申請中',
+    'FRONT_WAIT' => '受付待',
+    'FRONT_DENY' => '受付拒否',
+    'APPROVAL_WAIT' => '承認待',
+    'APPROVAL_DONE' => '承認済',
+    'APPROVAL_DENY' => '否認',
+
+    /* ステータス **/    
+    'REGIST_BUTTON' => '申請',
+    'CONFIRM_BUTTON' => '確認依頼',
+    'CONFIRM_DENY_BUTTON' => '申請拒否',
+    'FRONT_BUTTON' => '受付受領',
+    'FRONT_DENY_BUTTON' => '受付拒否',
+    'APPROVAL_BUTTON' => '承認',
+    'APPROVAL_DENY_BUTTON' => '否認',
+
+
+    /* ラベル **/
+    'hm_manage_system' => '工数管理システム',
+    'hm_manage_add' => '工数入力',
+    'Effort_List' => '工数一覧',
+    'Master_Maintenance' => 'マスタメンテ',
+    'LABEL_KUBUN' => '価格区分',
+    'LABEL_SLIP_DATE' => '対象日付',
+    'LABEL_STATUS' => '状態',
+    'LABEL_TUBE_FLAG' => '製品分類',
+    'LABEL_TUBE_FLAG_YES' => '造管',
+    'LABEL_TUBE_FLAG_NO' => '造管以外',
+    'LABEL_IS_FIXED' => 'FIX状態',
+
+    'LABEL_NO' => 'No',
+    'LABEL_ID' => 'ID',
+    'LABEL_ID_DETAIL' => '明細番号',
+    'LABEL_PROFIT_CODE' => '利益管理区分',
+    'LABEL_PROFIT_CODE_ORG' => '倉入登録/更新時',
+    'LABEL_ITEM_CATEGORY_CD' => '品目',
+    'LABEL_ITEM_CD' => '品目CD',
+    'LABEL_ITEM_NAME' => '品目名',
+    'LABEL_ITEM_CATEGORY' => '品目区分',
+    'LABEL_CATEGORY' => '種別',
+    'LABEL_ITEM_CATEGORY_RAW' => '原材料',
+    'LABEL_ITEM_CATEGORY_STOCK' => '貯蔵品',
+    'LABEL_UNIT' => '現行単価',
+    'LABEL_CHANGE_UNIT' => '変更単価',
+    'LABEL_DELETE' => '削除',
+    'LABEL_EDIT' => '編集',
+    'LABEL_SELECT' => '選択',
+    'LABEL_ADD' => '追加',
+    'LABEL_UPDATE_DATE' => '更新日時',
+    'LABEL_UPDATE_USER' => '更新者',
+    'LABEL_UPDATE_EMPLOYEE' => '社員番号',
+    'LABEL_SAVE' => '保存',
+    'LABEL_AREA_STORAGE' => 'エリア<br>(保管場所)',
+    'LABEL_AREA' => 'エリア',
+    'LABEL_AREA_L' => 'エリア(保管場所)',
+    'LABEL_FACILITY' => '設備',
+
+    'LABEL_SAVE_MOVE' => '利益管理区分<br>場所移動保存',
+    'LABEL_SAVE_HOUSEWARE_OUT' => '倉出保存',
+
+    'LABEL_LOCATION_NO' => '保管場所の棚番号等',
+    'LABEL_LOCATION_NO_S' => '棚番号等',
+    'LABEL_ASSET_NO' => '管理番号',
+    'LABEL_PURCHASE_QUANTITY' => '納入数量',
+    'LABEL_UNIT_PRICE' => '単価',
+    'LABEL_TOTAL_PRICE' => '金額',
+    'LABEL_JPY_CURRENCY' => '円換算',
+    'LABEL_JPY' => 'JPY',
+    'LABEL_RATE' => 'Rate',
+
+    'LABEL_PROFIT_CODE_S' => '利益管理区分',
+    'LABEL_SLOW_TERM_S' => '長期',
+
+    'LABEL_INCIDENTAL_EXPENSE_DISTRIBUTION' => '附帯費用(按分後)',
+    'LABEL_INCIDENTAL_EXPENSE_TOTAL' => '附帯費用(総額)',
+    'LABEL_EVIDENCE' => '伝票番号',
+    'LABEL_EXCHANGE_RATE' => '円換算レート',
+    'LABEL_COMMENT1' => '備考',
+    'LABEL_USD' => 'USD',
+    'LABEL_EUR' => 'EUR',
+    'LABEL_SETTLEMENT_DATE' => '支払日',
+    'LABEL_USD_JPY' => 'USD/JPY',
+    'LABEL_EUR_JPY' => 'EUR/JPY',
+    
+    'LABEL_PROC_CHARGE' => '担当者',
+    'LABEL_PROC_DATE' => '処理日',
+
+    'LABEL_CHANGE' => '変更',
+    'LABEL_REGST_NO' => '受付番号',
+    'LABEL_REGST_DATE' => '受付日',
+    'LABEL_APP_DATE' => '適用年月',
+    'LABEL_WEB_EDI' => 'WebEDI',
+    'LABEL_DELIVERY_CD' => '発先',
+    'LABEL_DELIVERY_NAME' => '発先',
+    'LABEL_CHANGE_KUBUN' => '価格区分',
+    'LABEL_DOWNLOAD_NEW' => '新規登録用品目ダウンロード',
+    'LABEL_DOWNLOAD_UPDATE' => '登録済　更新用品目ダウンロード',
+    'LABEL_UPLOAD_NEW' => '新規登録用品目アップロード',
+    'LABEL_UPLOAD_UPDATE' => '登録済　更新用品目アップロード',
+
+    'LABEL_UPLOAD_FILE' => '証跡用ファイルアップロード',
+    'LABEL_CRRENT_FILE' => '証跡ファイル',
+
+    'LABEL_REGIST_COMMENT' => '申請コメント',
+    'LABEL_CONFIRM_COMMENT' => '確認者コメント',
+    'LABEL_REGIST_FRONT_DENY_COMMENT' => '受付コメント',
+    'LABEL_REGIST_APPROVE_DENY_COMMENT' => '承認者コメント',
+
+    'LABEL_FUNC' => '機能',
+    'LABEL_TARGET_YM' => '対象年月',
+    'LABEL_DOWNLOAD_FIRST_TIME' => '初回',
+    'LABEL_COUNT_FIRST_TIME' => '初回',
+    'LABEL_DOWNLOAD_SECOND_TIME' => '２回目以降',
+    'LABEL_COUNT_SECOND_TIME' => '２回目以降',
+    'LABEL_DOWNLOAD_FLAG' => 'ダウンロード',
+    'LABEL_BUTTON_DITAIL' => '詳細',
+    'LABEL_DUE_DATE' => '納入日',
+    
+
+    'LABEL_DELIVERY_YM' => '倉入年月',
+    'LABEL_PROC_YM' => '対象年月',
+
+    'LABEL_ALL_YM' => 'すべて',
+    'LABEL_INCLUDE_ZERO' => '在庫０を含む',
+    'LABEL_CARRY_OVER' => '繰越',
+    'LABEL_TOTAL_THIS_MONTH' => '当月合計',
+    'LABEL_TOTAL_THIS_STOCK' => '当月残',
+    'LABEL_COLUMN' => '項目',
+    'LABEL_EMPTY_AFTER_QUANTITY' => '理論値0以外で未更新のみ表示',
+    
+    
+    'LABEL_HOUSEWARE' => '倉入',
+    'LABEL_HOUSEWARE_OUT' => '倉出',
+    'LABEL_CONSUMPTION' => '消費',
+    'LABEL_FREE_SUPPLY' => '無償支給',
+    'LABEL_FEE_SUPPLY' => '有償支給',
+    'LABEL_RETURN' => '戻し',
+    'LABEL_STOCK' => '在庫',
+    'LABEL_CURRENT_STOCK' => '現在在庫数',
+    'LABEL_CLOSE_DISPLAY' => '閉じる',
+    'LABEL_LAST_DISPOSAL' => '前破棄',
+    'LABEL_JOB_NO' => '棚卸指示書No.',
+
+
+    'LABEL_TARGET_DAY' => '対象日',
+    'LABEL_SLIP_GERNE' => '伝票種類',
+    'LABEL_QUANTIFY' => '数量',
+    'LABEL_PAYMENT_DESTINATION' => '支給先',
+    'LABEL_PAYMENT_JPY' => '有償支給対象額(円)',
+
+    'LABEL_STOCK_QUANTITY' => '在庫数量',
+    'LABEL_LAST_PROCESS' => '前回処置',
+    'LABEL_PROCESS' => '処置',
+    'LABEL_CURRENT_PROCESS' => '今回処置',
+    'LABEL_PROCESS_QUANTTITY' => '処置数量',
+    'LABEL_REASON' => '理由',
+    'LABEL_CLOSE_MONTH' => '締め対象月',
+    'LABEL_TARGET_PROC' => '処理対象', // 2020/08/16
+    'LABEL_DETAIL' => '明細情報',
+    'LABEL_MOVE_DATE' => '移動日',
+    'LABEL_READONLY' => '参照',    
+
+    'LABEL_TANAOROSHI_INTERMEDIATE' => '棚卸指示書データ',
+    'LABEL_TANAOROSHI_STATUS' => '実地棚卸', 
+    'LABEL_FIOI_STATUS' => '長期・スロー', 
+    'LABEL_BALANCE_CHECK_STATUS' => '残高確認', 
+    'LABEL_SHIP_SLIP' => '倉入／倉出', 
+    'LABEL_SUB_CALCULATE' => '集計', 
+    'LABEL_PROC_STOCK_TANAOROSHI' => '棚卸処理',
+    'LABEL_CREATE_STOCK_TANAOROSHI' => '例外登録',
+    'LABEL_LIST_STOCK_TANAOROSHI' => '例外登録一覧',
+    'LABEL_LIST_STOCK_TANAOROSHI_COMMENT' => '※対象月内すべての例外登録情報が表示されます。',
+    'TITLE_EDIT_STOCK_EXCEPT_TANAOROSHI' => '例外編集',
+    'TITLE_EDIT_STOCK_EXCEPT_TANAOROSHI_READ_ONLY' => '例外登録参照',
+    
+    'BUTTON_CREATE' => '作成', 
+
+    'TANAOROSHI_LOGICAL_QUANTITY' => '理論在庫数',
+    'TANAOROSHI_ACTUAL_QUANTITY' => '実地棚卸数',
+    'TANAOROSHI_DIFFERENCE_QUANTITY' => '棚差',
+    'TANAOROSHI_QUANTITY' => '数量',
+    'TANAOROSHI_UNIT' => '単位',
+    'TANAOROSHI_COMMENT1' => '※最大表示件数は',
+    'TANAOROSHI_COMMENT2' => '件です。',
+    'MESSAGE_TANAOROSHI_CAPTION01'    => ' ※実数量を入れずに締め処理を行うと実在庫数は０となります。',
+    'LABEL_RAW_EDIT_COMMENT1' => '今月は変更できません',
+
+    'DELIVERY_DATE' => '納入日',
+    'SLIP_QUANTITY' => '数量',
+    'SLIP_UNIT' => '数量単位',
+    'QUANTITY' => '数量<br>（集計単位）',
+    'SLIP_UNIT_PRICE' => '単価（円）',
+    'SLIP_UNIT_PRICE_JPY' => '金額（円）',
+    'CURRENCY' => '単価',
+    'AMOUNT' => '合計金額',
+    'AMOUNT_JPY' => '金額（円）',
+    'RAW_AMOUNT_JPY' => '有償支給対象額(円)',
+
+    
+    'LABEL_SLIP_QUANTITY' => '倉入数量',
+    'LABEL_SLIP_AMOUNT' => '倉入金額',
+    'LABEL_TOTAL_QUANTITY' => '現在庫数',
+    'LABEL_TOTAL_SHIP_QUANTITY' => '総倉出数',
+    'LABEL_SHIP_QUANTITY' => '倉出数',
+    'LABEL_SHIP_SLIP_QUANTITY' => '倉入/倉出数',
+    'LABEL_PROFIT_CODE_TO' => '倉出先',
+    'LABEL_DESCRIPTION_NAME' => '伝票種類',
+
+
+    'LABEL_ADD_OUT' => '倉出追加',
+    'LABEL_BACK_ADD_OUT' => '戻し処理',
+    'LABEL_LIST_OUT' => '倉出明細一覧',
+    /* ボタン **/    
+    'BUTTON_ADD' => '追加',
+    'BUTTON_ADD_REGI' => '新規申請',
+    'BUTTON_DELETE' => '削除',
+    'BUTTON_SELECT' => '選択',
+    'BUTTON_SEARCH' => '検索',
+    'BUTTON_EDIT' => '編集',
+    'BUTTON_DETAIL' => '詳細',
+    'BUTTON_SAVE' => '保存',
+    'BUTTON_DOWNLOAD' => 'ダウンロード',
+    'BUTTON_UPLOAD' => 'アップロード',
+    'BUTTON_BULK_SOLO' => '一　括',
+    'BUTTON_BULK' => '一括処理',
+    'BUTTON_SLIP_DELETE' => '伝票削除',
+    'BUTTON_SLIP_EDI_DOWNLOAD' => 'Web EDI用<br>ダウンロード',
+    'BUTTON_PREVIOUS' => '戻　る',
+    'BUTTON_DISTRIBUTE' => '按　分',
+    'BUTTON_CREATE_STOCK_SLIP' => '新規倉入登録',
+    'BUTTON_UPDATE' => 'データ更新',
+    'BUTTON_GO' => '実行',
+    'BUTTON_RELESE' => '解除',
+    'BUTTON_CREATE_STOCK_TANAOROSHI' => '例外登録',
+
+    'BUTTON_REGIST' => '登録',
+    'BUTTON_CANCEL' => 'キャンセル',
+
+    'BUTTON_EXTRACT' => '反映',
+
+    'TITLE_HOUSEWARE_OUT_DATE' => '倉出日',
+    'TITLE_HOUSEWARE_OUT_PRICE_JPY' => '倉出金額(JPY)',
+    'TITLE_HOUSEWARE_OUT_COMMENT' => '用途',
+    'TITLE_HOUSEWARE_BACK_OUT_DATE' => '倉出戻し日',
+    'TITLE_BACK_SHIP_QUANTITY' => '戻し数',
+    'TITLE_GO' => '処理実行',
+    
+    /* 画面タイトル **/    
+    'TITLE_LIST_DIVISIONS' => '部門選択',
+    'TITLE_LIST_MENU' => 'メニュー',
+    'TITLE_LIST_SLIP' => '販売単価変更',
+    'USER_MASTER_MENU' => 'ユーザーマスタ',
+    'TOKUISAKI_ENTRY' => '得意先エントリ',
+    'TITLE_LIST_SLIP_L' => '伝票一覧',    
+    'TITLE_LIST' => '一覧',
+    'TITLE_LIST_SLIP_CPPU' => '単価変更管理',
+    'TITLE_LIST_UNIT' => '単価変更',
+    'TILTE_SHOW_SLIP' => '伝票作成',
+    'TITLE_UNIT_INOUT'  => 'ダウンロード/アップロード',
+    'TITLE_UNIT_INOUT_FOR_WEB_EDI'  => 'Web-EDI用ダウンロード',
+    'TITLE_BULK' => '一括処理及び証跡ファイルアップロード',
+
+    'TITLE_LIST_INVENTORY_MANAGEMENT' => '締め処理',
+    'TITLE_LIST_HOUSEWARE' => '倉入台帳',
+    'TITLE_LIST_HOUSEWARE_RMAS' => '倉入台帳',
+    'TITLE_LIST_HOUSEWARE_RMAS_ADD' => '倉入追加',
+    'TITLE_LIST_HOUSEWARE_RMAS_OUT' => '原材料倉出台帳',
+    'TITLE_LIST_HOUSEWARE_RMAS_OUT_STORED' => '貯蔵品倉出台帳',
+    'TITLE_LIST_HOUSEWARE_MENU' => '原材料／貯蔵品',
+    
+    'TITLE_HELP_ONE' => 'ヘルプ・説明ページ',
+
+    'TITLE_LIST_BALANCE_LIST' => '集計情報',
+
+    'TITLE_LIST_RMAS_MENU' => '原材料・貯蔵品の倉入・倉出管理',
+    'TITLE_LIST_TANA_SLOW_MENU' => '実棚卸登録・評価替え登録',
+    'LABEL_LIST_SLOW_MENU' => '長期・スロー品',
+    'LABEL_LIST_SLOW_MENU_SUB' => '(製品・原材料・貯蔵品)',
+    'LABEL_LIST_CAL_MENU_SUB' => '(残高確認)',
+    'LABEL_LIST_TANAOROSHI_MENU' => '実棚数量登録',
+    'LABEL_LIST_TANAOROSHI_MENU_SUB' => '(原材料・貯蔵品)',
+    'TITLE_LIST_TANAOROSHI_MENU' => '実棚卸用メニュー',
+    'TITLE_LIST_TANAOROSHI_IPUT' => '実棚登録一覧',
+    
+
+    'TITLE_CURRENT_TOTAL_QTY' => '当月残高確定数量',
+    'TITLE_LIST_CURRENT_UNIT_PRICE' => '当月評価単価',
+    'TITLE_CURRENT_TOTAL_AMOUNT_JPY' => '当月残高確定金額',
+ 
+    'TITLE_CURRENT_SLIP_QTY' => '当月倉入数量',
+    'TITLE_CURRENT_SLIP_AMOUNT_JPY' => '当月倉入金額（円）',
+
+    'TITLE_CURRENT_SHIP_QTY' => '当月倉出数量',
+    'TITLE_CURRENT_SHIP_AMOUNT_JPY' => '当月倉出金額（円）',
+    'TITLE_CURRENT_DIFF_QTY' => '当月棚差数量',
+    'TITLE_CURRENT_DIFF_AMOUNT_JPY' => '当月棚差金額',
+
+    'TITLE_SLOW_BEFORE_QTY' => '長期・スロー対応前の当月末数量',
+    'TITLE_SLOW_AFTER_QTY' => '長期・スロー対応後の当月末数量',
+    'TITLE_SLOW_BEFORE_AMOUNT_JPY' => '長期・スロー対応前の当月末金額',
+    'TITLE_SLOW_AFTER_AMOUNT_JPY' => '長期・スロー対応後の当月末金額',
+
+    'TITLE_SLOW_AFTER_CHANGE_QTY' => '評価替え数量',
+    'TITLE_SLOW_AFTER_CHANGE_AMOUNT_JPY' => '評価替えによる評価損',
+
+    'TITLE_SLOW_AFTER_DISPOSAL_QTY' => '破棄数量',
+    'TITLE_SLOW_AFTER_DISPOSAL_LOASS_AMOUNT_JPY' => '破棄による評価損',
+
+    'TITLE_LAST_TOTAL_QTY' => '前月残高数量',
+    'TITLE_LAST_UNIT_PRICE_JPY' => '前月評価単価',
+    'TITLE_LAST_TOTAL_AMOUNT_JPY' => '前月残高金額',
+
+    'TITLE_LIST_SLOW' => '長期・スロー品',
+    'LABEL_SLOW_YM' => '実地棚卸月',
+    'LABEL_SLOW_TERM' => '長期／スロー',
+    'LABEL_ITEM_CATEGORY_NAME' => '品目区分',
+    'LABEL_UPDATE_ITEMS' => '変更品目',
+    'TITLE_LIST_SLOW_MANAGE' => '長期・スロー品管理一覧',
+    'TILTE_SHOW_SLOW_MANAGE' => '処理対象月追加',
+    'LABEL_PRODUCT' => '製品データ反映',
+    'LABEL_PROC_YM_DATE' => '対象月',
+    'LABEL_BLUK_RAW_SUPPLY' => '原材料・貯蔵品データ反映',
+    'LABEL_ACCOUNT_CLOSE' => '締め処理',
+    'LABEL_ACCOUNT_CLOSE_RELEASE' => '締め処理解除',
+    'LABEL_SLOW_MANAGE_YM' => '長期滞留品処理月',
+    'LABEL_PROCESSED' => '処理済み', 
+    'LABEL_WAIT' => '処理待ち', 
+    'LABEL_ONGOING' => '処理中', 
+    'LABEL_UNPROCESSED' => '未処理',    
+    'LABEL_TANAOROSHI_OUT' => '反映',
+    'LABEL_CLOSE' => '締め',
+    'LABEL_LAST_CLOSE' => '月締め',
+    'LABEL_CLOSE_RELEASE' => '解除',
+    'LABEL_RMAS_MANAGE_YM' => '会計月',
+    
+    'LABEL_SORT' => '並び順',
+
+    'TILTE_RMAS_MANAGE_ADD' => '会計月設定',
+
+    'TITLE_LIST_REGIST_SLIP' => '倉入処理',
+    'TITLE_LIST_CREATE_STOCK_SLIP' => '倉入登録',
+    'TITLE_LABEL_CLIENT_TO' => '仕入先名',
+    'TITLE_HOUSEWARE' => '倉入',
+    'TITLE_HOUSEWARE_OUT' => '倉出',
+    'TITLE_LIST_EDIT_STOCK_SLIP' => '倉入編集',
+    'TITLE_LIST_READ_ONLY_STOCK_SLIP' => '倉入参照',
+    'TITLE_LINK_HELP' => 'HELP',
+    
+    'TITLE_AGGREGATION' => '集計',
+    
+	
+
+
+    'MESSAGE_REQUIRRED' => '項目は必須入力になります。',
+
+    'MESSAGE_SLIP_OUT_CONSUMPTION' => '伝票種類：消費の場合、倉出先は必須です　　　　　',
+    'MESSAGE_SLIP_OUT_SUPPLIED'    => '伝票種類：有償／無償支給の場合、支給先は必須です',
+
+    
+    
+    
+];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">@lang('message.hm_manage_system')</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <a id='list' href="{{ route('add_top') }}"><i class="fa fa-list" aria-hidden="true"></i><span>@lang('message.hm_manage_add')</span></a>
+                </div>
+                <div class="card-body">
+                    <a id='list' href="/cppu/user_master_list"><i class="fa fa-list" aria-hidden="true"></i><span>@lang('message.Effort_List')</span></a>
+                </div>
+                <div class="card-body">
+                    <a id='list' href="/cppu/user_master_list"><i class="fa fa-list" aria-hidden="true"></i><span>@lang('message.Master_Maintenance')</span></a>
                 </div>
             </div>
         </div>
